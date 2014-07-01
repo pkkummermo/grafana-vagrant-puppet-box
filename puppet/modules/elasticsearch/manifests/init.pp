5e666e8938bf61e9ec926fb556d31004479b78f3
Per Kristian Kummermo <per-kristian.kummermo@telenor.com>
2014-07-01 08:58:38 +0200

    Added new module: Elasticsearch

---
+++ puppet/modules/elasticsearch/manifests/init.pp
@@ -0,0 +1,56 @@
+class elasticsearch {
+
+  $build_dir = "/tmp"
+
+  $elasticsearch_url = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.tar.gz"
+  $elasticsearch_loc = "$build_dir/elasticsearch-1.2.1.tar.gz"
+  $elasticsearch_dest = "/opt/elasticsearch/"
+  $elasticsearch_log = "/var/log/elasticsearch"
+  $elasticsearch_init = "/etc/init.d/elasticsearch"
+
+  package { "openjdk-7-jre" :
+    ensure => latest,
+  }
+
+  file { $elasticsearch_init :
+    source => "puppet:///modules/elasticsearch/elasticsearch.sh",
+    ensure => present
+  }
+
+  file { $elasticsearch_log :
+    ensure => directory,
+    owner => www-data,
+    group => www-data,
+  }
+
+  file { $elasticsearch_dest :
+    ensure => directory,
+    owner => www-data,
+    group => www-data,
+  }
+
+  exec { "download-elasticsearch":
+    command => "wget -O $elasticsearch_loc $elasticsearch_url",
+    creates => "$elasticsearch_loc"
+  }
+
+  exec { "unpack-elasticsearch":
+    command => "tar -zxvf $elasticsearch_loc",
+    cwd => $build_dir,
+    subscribe => Exec[download-elasticsearch],
+    refreshonly => true,
+  }
+
+  exec { "install-elasticsearch" :
+    command => "mv elasticsearch-1.2.1 $elasticsearch_dest",
+    cwd => "$build_dir",
+    require => Exec[unpack-elasticsearch],
+    creates => "/opt/elasticsearch",
+    notify => Service[elasticsearch],
+  }
+
+  service { elasticsearch :
+    ensure  => running,
+    require => File[$elasticsearch_init]
+  }
+}
