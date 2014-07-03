efa86c1b3c464123b911fd0e03bd12267fef95bf
Per Kristian Kummermo <per-kristian.kummermo@telenor.com>
2014-07-03 14:19:28 +0200

    Graphtie locale: Setting Europe/Oslo as default

--- puppet/modules/graphite/files/local_settings.py
+++ puppet/modules/graphite/files/local_settings.py
@@ -4,7 +4,7 @@
 #DEBUG = True
 
 # Set your local timezone (django will try to figure this out automatically)
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = 'Europe/Oslo'
 
 # Setting MEMCACHE_HOSTS to be empty will turn off use of memcached entirely
 #MEMCACHE_HOSTS = ['127.0.0.1:11211']
